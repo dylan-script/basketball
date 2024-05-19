c1de650c6670ae35916f7082d3a41f1c1529bb77
dylan-script <63126577+dylan-script@users.noreply.github.com>
2024-05-19 17:04:47 -0500

    Etiquetas meta con la dependencia Helmet

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@
 import "./App.css";
 import { Header } from "./components/header/header";
 import { Body } from "./components/body/body";
+import Helmet from "react-helmet";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Helmet>
+        <title>App Title</title>
+        <meta name="author" content="Dilan, Nicolas"></meta>
+        <meta name="keywords" content="react"></meta>
+        <meta
+          name="description"
+          content="proyecto final sobre un equipos de baloncesto con React"
+        />
+      </Helmet>
       <Header></Header>
       <Body></Body>
     </div>
